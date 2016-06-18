3dee37e3ee62458dc138441800449548e2938c02
Hack Reactor Students <accounts@hackreactor.com>
2016-06-18 11:19:04 -0700

    bar graphing

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-// {API Base}/demographic/{dataVersion}/coordinates?latitude={latitude}&longitude={longitude}&format={format}&callback={functionName}
 
+var dataOfdata = data.map(function(curr){
+  var obj = {};
+  obj.year = curr[11];
+  obj.age = curr[12];
+  obj.race = curr[9];
+  return obj;
+});
 
 
 $( document ).ready(function() {
-	$.ajax({
-	    url: 'http://example.com/',
-	    type: 'GET',
-	    data: 'ID=1&Name=John&Age=10', // or $('#myform').serializeArray()
-	    success: function() { alert('GET completed'); }
-	});
+
+d3.select('.holder').selectAll('div')
+              .data(dataOfdata)
+              .enter()
+              .append('div')
+              .style('height', function(d) {
+                return d.age;
+              })
+              .style('fill', 'blue')
+              .text(function(d) {
+                return d.year;
+              });       
+
+  // $.ajax({
+	//     url: 'http://example.com/',
+	//     type: 'GET',
+	//     data: 'ID=1&Name=John&Age=10', // or $('#myform').serializeArray()
+	//     success: function() { alert('GET completed'); }
+	// });
 
 
 
